341cb6bedd1b2218a8f7f9a21f7b344a00176960
Marilu09 <marianagaravito9809@gmail.com>
2022-09-08 20:34:19 -0500

    Rutas GET y POST

--- Knitheart/miembros/urls.py
+++ Knitheart/miembros/urls.py
@@ -3,6 +3,8 @@ from unicodedata import name
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('agregar', views.newMembers, name= 'agregar'),
--- Knitheart/miembros/views.py
+++ Knitheart/miembros/views.py
@@ -1,18 +1,38 @@
+import email
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotAllowed
+from .models import Members
 
 def index (request):
     return HttpResponse ("Hola, mundo")
 
 def newMembers (request):
     if request.method == 'POST':
+        data = json.loads(request.body)
+        member = Members( 
+            name = data ["name"],
+            email= data ["email"]
+        )
+        member.save()
+
         return HttpResponse ("va a agregar un nuevo miembro")
     else:
         return HttpResponseNotAllowed (['POST'],"metodo invalido")
 
 def getMembers (request):
     if request.method == 'GET':
-        return HttpResponse ("esta es la inf de los miembros")
+        members = Members.objects.all()
+        allMembersData = []
+        for x in members:
+            data = {"id": x.id, "name": x.name, "email": x.email}
+            allMembersData.append(data)
+        dataJson = json.dumps(allMembersData)
+        #print(dataJson)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = "text/json"
+        resp.content = dataJson
+        return resp
     else:
         return HttpResponseNotAllowed (['GET'],"metodo invalido")        
 
